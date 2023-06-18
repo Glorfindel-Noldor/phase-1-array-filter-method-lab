4f38d5e6eef7d8fdb408cccfb9657b9d2ab3f07c
Bogar Martinez <bogar.cm@icloud.com>
2023-06-18 15:52:48 -0500

    filter() with the help of  =>

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(parameter, name) {
+  return parameter.filter(x => x.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(parameter, letters) {
+      return parameter.filter(x =>  x.startsWith(letters));
+}
+
+function matchName(parameter, name){
+return parameter.filter(x => x.name=== "Bobby")
+}
